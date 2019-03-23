14238a814242bdf0d6e54a8231885b9491d22952
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 2ac10a882be1f12c0efaaedbbd2ef0283db0e613
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:16:43 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637
        (cherry picked from commit 1fe2fee63e99498f4d18baba92756e3c8185be92)
    
    commit 5c8ba8ffab8c1b11b1a8861364b70428f8d962c9
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:43:51 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08
        (cherry picked from commit 958b572b29f2216d952bf4a651d9d997f5e18e32)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit caad4a0dddd28a1f97e2395086e6a4b623e8db3c)

---
+++ calyx_flame.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/device-calyx.mk)
+$(call inherit-product, device/google/coral/aosp_flame.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_flame
+PRODUCT_MODEL := Pixel 4
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=flame \
+    PRIVATE_BUILD_DESC="flame-user 13 TP1A.220624.014 8819323 release-keys"
+
+BUILD_FINGERPRINT := google/flame/flame:13/TP1A.220624.014/8819323:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
