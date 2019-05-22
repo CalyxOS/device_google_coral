72e741014ec15c7f8c5cf1bf7461836e31c11a24
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-22 15:13:23 -0700

    Add privapp permissions for Google EUICC apps
    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,3 +1,4 @@
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/coral/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
