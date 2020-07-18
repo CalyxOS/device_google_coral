d0d730319a7dd1051ed28b17f93f00476fe15340
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    floral: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does support HBM (right now), but elsewhere.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
     * AOSP declares `ro.vendor.display.foss` but we don't
       have support for IAdaptiveBacklight, so don't declare.
    
    Change-Id: Ia96134e0a4bb06269b48671a87991d5e7a53d9ed

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -6,7 +6,15 @@
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Manifests
+DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/calyx/config/device_framework_matrix.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/vendor
