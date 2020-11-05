1d4df00419e17253d7bba36ca241e506ca2fe8b5
Danny Lin <danny@kdrag0n.dev>
2020-11-05 15:01:18 -0800

    Build Active Edge service (ElmyraService)
    This package provides comprehensive support for the Active Edge squeeze
    gesture supported on this device.
    
    Change-Id: I2158c22e727e293e3f02b9ddf614a943848ad587
    (cherry picked from commit 3ab661978956c85635c2386784a12a4e50f4a208)

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,9 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/coral/overlay-calyx
 
+# Elmyra
+PRODUCT_PACKAGES += \
+    ElmyraService
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
