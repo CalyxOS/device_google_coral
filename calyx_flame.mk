1fe2fee63e99498f4d18baba92756e3c8185be92
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-12 03:16:43 +0530

    calyx: Inherit device-calyx.mk
    Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637

--- calyx_flame.mk
+++ calyx_flame.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, vendor/calyx/config/common.mk)
 
 # Inherit device configuration
+$(call inherit-product, device/google/coral/device-calyx.mk)
 $(call inherit-product, device/google/coral/aosp_flame.mk)
 
 ## Device identifier. This must come after all inclusions
