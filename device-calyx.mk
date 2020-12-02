a794247995898ed9bfbd00d29439b15bb87f919e
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-03 03:43:51 +0530

    Add overlay-calyx to the list of overlays
    Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/coral/device-calyx.mk
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
