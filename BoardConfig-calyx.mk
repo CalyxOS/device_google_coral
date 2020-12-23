c5d5aac9fc079ad7f07c821e20feda1a65a88709
Peter Cai <peter@typeblog.net>
2020-12-23 17:09:47 +0800

    floral: Set up proprietary files extraction & build vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I17f79b417a8f0d022bec9c6b5f37d03b842d43e1
    (cherry picked from commit 7903f7110a179ed44842a2ea5b7b49daffa6c9d1)

---
+++ BoardConfig-calyx.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
