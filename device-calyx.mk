76c4f5c3271cabd7ca0523c7f220c5ebf5d0bbb5
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:28 +0530

    floral: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097
    (cherry picked from commit 69fb890a588f90c1d05ac0101d3b537e626c509f)

--- device-calyx.mk
+++ device-calyx.mk
@@ -5,3 +5,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/coral/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
     device/google/coral/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/coral/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
