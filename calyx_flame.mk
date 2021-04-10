23a447e0b9d2f9d5d0549dfc7c4ae45bb61f52b5
Michael Bestas <mkbestas@gmail.com>
2021-04-10 14:41:47 +0300

    calyx: Set PRODUCT_BRAND to `google`
    Change-Id: Iefd6a13a161b4f0ee77136f79325e543173e98f1

--- calyx_flame.mk
+++ calyx_flame.mk
@@ -8,5 +8,7 @@ $(call inherit-product, device/google/coral/aosp_flame.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_flame
 PRODUCT_MODEL := Pixel 4
+PRODUCT_BRAND := google
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS :=
