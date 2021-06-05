a7bf99a0ddabe3ce282f795c769770743fcc0580
Eamon Powell <eamonpowell@outlook.com>
2021-06-05 16:41:48 +1000

    floral: Add separated overlay directories for coral & flame
    We need to add specific overlays for each device now. Add
    a device specific overlay directory for both coral & flame.
    
    Change-Id: I4d1af6dc4eb0491a071f428781095b92a707c075

---
+++ coral/device-calyx.mk
@@ -0,0 +1,3 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/coral/coral/overlay-calyx
+
+$(call inherit-product, device/google/coral/device-calyx.mk)
---
+++ flame/device-calyx.mk
@@ -0,0 +1,3 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/coral/flame/overlay-calyx
+
+$(call inherit-product, device/google/coral/device-calyx.mk)
