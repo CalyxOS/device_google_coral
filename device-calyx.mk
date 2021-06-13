69ab494d880ae28edb63330bf59e273236314bbf
Michael Bestas <mkbestas@gmail.com>
2021-06-14 02:13:36 +0300

    Move euicc permission to product partition
    Change-Id: Iecd09978aa62e74bf82fc18aa915b6ece715b56f

--- device-calyx.mk
+++ device-calyx.mk
@@ -6,7 +6,7 @@ PRODUCT_PACKAGES += \
 
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/coral/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
 
 # For Google Camera
