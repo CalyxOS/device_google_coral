1c96d01824c7cb1e1f1af85d8ac6f76c382520ad
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:37:17 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 0f61086c0e40af37530a5bbd472d825d16b088e9)

--- calyx_coral.mk
+++ calyx_coral.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_coral
 PRODUCT_MODEL := Pixel 4 XL
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=coral \
+    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/coral/coral-vendor.mk)
