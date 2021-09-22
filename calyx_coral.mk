ae234b88f9d49882ad243fcb0689efde1e4a6047
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:37:17 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 0f61086c0e40af37530a5bbd472d825d16b088e9)

--- calyx_coral.mk
+++ calyx_coral.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_coral
 PRODUCT_MODEL := Pixel 4 XL
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=coral \
+    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.211205.008 7888514 release-keys"
+
+BUILD_FINGERPRINT := google/coral/coral:12/SQ1A.211205.008/7888514:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/coral/coral-vendor.mk)
--- calyx_flame.mk
+++ calyx_flame.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_flame
 PRODUCT_MODEL := Pixel 4
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=flame \
+    PRIVATE_BUILD_DESC="flame-user 12 SQ1A.211205.008 7888514 release-keys"
+
+BUILD_FINGERPRINT := google/flame/flame:12/SQ1A.211205.008/7888514:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/flame/flame-vendor.mk)
