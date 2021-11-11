ce7907f821e0c34776c2eb4193cd5f3cd45a9a7f
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    device-calyx: Set TARGET_PREBUILT_KERNEL
    Change-Id: Idf497f71284dc28b2d9f700d5add3b8b1e52f975

--- device-calyx.mk
+++ device-calyx.mk
@@ -13,3 +13,5 @@ PRODUCT_COPY_FILES += \
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/coral/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+TARGET_PREBUILT_KERNEL := device/google/coral-kernel/Image.lz4
