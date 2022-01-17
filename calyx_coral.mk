0ece5e5cd5e286c420f1aed16114da13c8e05242
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-18 03:25:00 +0530

    calyx: Update build desc and fingerprint to Jan 2022
    Change-Id: I6709cc93f3f3d9439d3fbaf14f724d6c8fe3a4e9

--- calyx_coral.mk
+++ calyx_coral.mk
@@ -12,9 +12,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=coral \
-    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.211205.008 7888514 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.220105.002 7961164 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:12/SQ1A.211205.008/7888514:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:12/SQ1A.220105.002/7961164:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
